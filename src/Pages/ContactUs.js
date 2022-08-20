4cc3c96fa4c2b72350ac7946c0042513a40ed023
Shubh <shubh410shekhar@gmail.com>
2022-08-20 14:19:21 +0530

    Contact POST

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -2,21 +2,38 @@
 import "./ContactUs.css";
 
 const ContactUs = () => {
+   async function contactFromHandler(e) {
+        e.preventDefault();
+        const contactDetails = {
+            name: e.target[0].value,
+            email: e.target[1].value,
+            phone: e.target[2].value
+        }
+
+        const response = await fetch('https://shubh-s-ecommerce-default-rtdb.firebaseio.com/ecommerce/contact.json', {
+            method: 'POST',
+            body: JSON.stringify(contactDetails),
+        })
+        const data = await response.json()
+    console.log(data);
+        
+    }
   return (
-   <form className="contact-form">
+   <form className="contact-form" onSubmit={contactFromHandler}>
+    <h1>Please Fill Your Details👇</h1>
     <div>
         <label htmlFor="name">Name</label><br/>
-        <input type='text' id='name' placeholder="Enter Your Name"/>
+        <input type='text' id='name' name placeholder="Enter Your Name"/>
     </div>
     <div>
         <label htmlFor="email">Email</label><br/>
         <input type='email' id='email' placeholder="Enter Your Email"/>
     </div>
     <div>
-        <label htmlFor="email">Phone</label><br/>
-        <input type='number' id='phone' placeholder="Enter Your Email"/>
+        <label htmlFor="phone">Phone</label><br/>
+        <input type='number' id='phone' placeholder="Enter Your Phone Number"/>
     </div>
-    <button>Submit</button>
+    <button type='submit'>Submit</button>
    </form>
   );
 };
